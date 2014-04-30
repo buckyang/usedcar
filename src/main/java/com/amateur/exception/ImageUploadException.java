9058b38721d1b1be970ec624482d41691a294770
coldrain <hecoldrain@qq.com>
2014-04-30 09:12:44 +0800

    add image upload exception class.

---
+++ src/main/java/com/amateur/exception/ImageUploadException.java
@@ -0,0 +1,10 @@
+package com.amateur.exception;
+
+public class ImageUploadException extends Exception {
+
+	private static final long serialVersionUID = -5435013288440506136L;
+
+	public ImageUploadException(String msg) {
+		super(msg);
+	}
+}
