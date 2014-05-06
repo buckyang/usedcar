7401d3a1657b5d7a4f57ffadabf85f69670334da
buckyang <bbuckyang@gmail.com>
2014-05-06 00:23:39 -0700

    database design update

---
+++ src/main/resources/SQL/05052014_ddl_menu_static_page_verification_code.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS `usedcar`.`menu` (
+  `menu_id` INT(11) NOT NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `order` TINYINT(4) NULL DEFAULT NULL,
+  `link_url` VARCHAR(200) NULL DEFAULT NULL,
+  `parent_menu_id` INT(11) NOT NULL,
+  PRIMARY KEY (`menu_id`),
+  INDEX `fk_menu_menu1_idx` (`parent_menu_id` ASC),
+  CONSTRAINT `fk_menu_menu1`
+    FOREIGN KEY (`parent_menu_id`)
+    REFERENCES `usedcar`.`menu` (`menu_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `usedcar`.`static_page` (
+  `page_id` VARCHAR(20) NOT NULL,
+  `url` VARCHAR(500) NULL,
+  `creation_date` TIMESTAMP NULL,
+  `name` VARCHAR(100) NULL,
+  `description` VARCHAR(500) NULL,
+  `start_date` TIMESTAMP NULL,
+  `end_date` TIMESTAMP NULL,
+  `enabled` TINYINT(1) NULL,
+  `keyword` VARCHAR(500) NULL,
+  `title` VARCHAR(500) NULL,
+  `content` TEXT NULL,
+  `rich_text_enabled` TINYINT(1) NULL,
+  `last_modified` TIMESTAMP NULL,
+  `rank` TINYINT NULL,
+  `type` TINYINT NULL,
+  PRIMARY KEY (`page_id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `usedcar`.`verification_code` (
+  `id` VARCHAR(20) NOT NULL,
+  `type` TINYINT NULL,
+  `verification_code` VARCHAR(45) NULL,
+  `valid_date` TIMESTAMP NULL,
+  `used` TINYINT(1) NULL,
+  `principle` VARCHAR(45) NULL,
+  `used_date` TIMESTAMP NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
